9b435d351b1d6041f7abab7f06d922cd3431e812
Thibault Duval <gerardufoin2@hotmail.com>
2017-04-19 21:27:32 +0200

    Change curtains into animations; Reorder Animations folder

--- Assets/Scripts/Levels/Managers/GameManager.cs
+++ Assets/Scripts/Levels/Managers/GameManager.cs
@@ -4,12 +4,9 @@ using System.Collections;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject m_curtains;
+    private Animator m_curtains;
 
     private RecepterManager _recepterManager;
-
-    private Vector3 _curtainsDest;
-    private float _startTimer = 0.0f;
 
     private int _placedPieces;
 
@@ -17,23 +14,7 @@ public class GameManager : MonoBehaviour
 	void Start ()
     {
         _recepterManager = FindObjectOfType<RecepterManager>();
-        _curtainsDest = m_curtains.transform.position;
-        _curtainsDest.y = 11.0f;
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if (_startTimer < 1.0f)
-        {
-            _startTimer += Time.deltaTime;
-            return ;
-        }
-
-	    if (m_curtains.transform.position != _curtainsDest)
-        {
-            m_curtains.transform.position = Vector3.MoveTowards(m_curtains.transform.position, _curtainsDest, 8.0f * Time.deltaTime);
-        }
+        m_curtains.SetTrigger("Open");
 	}
 
     public void PlacePiece()
@@ -41,6 +22,7 @@ public class GameManager : MonoBehaviour
         _placedPieces++;
         if (_placedPieces >= _recepterManager.RecepterCount)
         {
+            m_curtains.SetTrigger("Close");
             Debug.Log("FINISHED :D");
         }
     }
