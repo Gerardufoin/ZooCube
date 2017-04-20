69937e7f5c15b0688928c5c81f842fe631e1faa5
Thibault Duval <gerardufoin2@hotmail.com>
2017-04-20 22:04:13 +0200

    Add begining of the victory screen

--- Assets/Scripts/Levels/Managers/GameManager.cs
+++ Assets/Scripts/Levels/Managers/GameManager.cs
@@ -3,25 +3,44 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    // Curtains of the scene
     [SerializeField]
     private Animator m_curtains;
+    // Reference to the victory screen in the canvas
+    [SerializeField]
+    private Animator m_victoryScreen;
+    // Reference to the star particle system
+    [SerializeField]
+    private ParticleSystem m_starParticles;
 
+    // Reference to the RecepeterManager script
     private RecepterManager _recepterManager;
 
+    // Number of placed pieces by the player
     private int _placedPieces;
 
-	// Use this for initialization
-	void Start ()
+    // We disable the victory screen
+    private void Awake()
+    {
+        m_victoryScreen.gameObject.SetActive(false);
+    }
+
+    void Start ()
     {
         _recepterManager = FindObjectOfType<RecepterManager>();
+        // Opening of the curtains
         m_curtains.SetTrigger("Open");
 	}
 
+    // Called when the player place a piece. If all the pieces are placed, we call the victory screen and close the curtains
     public void PlacePiece()
     {
         _placedPieces++;
         if (_placedPieces >= _recepterManager.RecepterCount)
         {
+            m_starParticles.Play();
+            m_victoryScreen.gameObject.SetActive(true);
+            m_victoryScreen.SetTrigger("Appear");
             m_curtains.SetTrigger("Close");
             Debug.Log("FINISHED :D");
         }
