24b2d7b55ea30c3e0e369db13dc899ad0976e981
Thibault Duval <gerardufoin2@hotmail.com>
2017-04-21 22:02:08 +0200

    Add begining of main menu

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void Play()
+    {
+        Debug.Log("PLAY");
+    }
+
+    public void Editor()
+    {
+        Debug.Log("EDITOR");
+    }
+
+    public void Options()
+    {
+        Debug.Log("OPTIONS");
+    }
+
+    public void Credits()
+    {
+        Debug.Log("CREDITS");
+    }
+}
